d19d514ea245571e1a23b7c1e8b51cec90152f38
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-05-21 00:43:29 +0300

    Adding files.

--- Run.bat
+++ Run.bat
@@ -1,3 +1,4 @@
-mingw32-make All
+gcc -o out main.c ./DataAccess/MemoryManagement/Memory_Management.c ./DataAccess/FileAccess/File_Access.c ./DataAccess/DataManagement/Data_Management.c ./Application/Student/student.c ./Application/Admin/admin.c  ./UserInterface/IO/Input_program.c ./UserInterface/IO/Output_program.c ./UserInterface/Menu/Menu_program.c Error_Handling.h ./Application/Authentication/Authentication_program.c
 
 out.exe
+
